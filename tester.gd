5769efd4cbb882b6f400797cf96e32923b370862
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2019-02-21 08:11:02 +0100

    Add testing system

---
+++ tester.gd
@@ -0,0 +1,54 @@
+extends Node
+
+var current = null
+var tests = []
+var frame_time = 0.0
+var fixed_time = 0.0
+
+func _ready():
+	var dirs = ["tests"]
+	while not dirs.empty():
+		var d = dirs.pop_front()
+		_expand(d, dirs)
+	print("Running %d tests:\n%s" % [tests.size(), tests])
+
+func _process(delta):
+	frame_time += delta
+	_check_end()
+
+func _physics_process(delta):
+	fixed_time += delta
+	_check_end()
+
+func _check_end():
+	if current == null:
+		if tests.size() == 0:
+			_end_tests()
+			return
+		current = load(tests.pop_front()).new()
+		add_child(current)
+	elif current._quit:
+		current.queue_free()
+		current = null
+
+func _end_tests():
+	print("Tests ends")
+	set_process(false)
+	set_physics_process(false)
+	get_tree().quit()
+
+func _expand(p_name, r_dirs):
+	var dir = Directory.new()
+	dir.open("res://")
+	if dir.change_dir(p_name) != OK:
+		print("Unable to chdir into: %s" % p_name)
+		return
+	dir.list_dir_begin(true, true)
+	var f = dir.get_next()
+	while f != "":
+		if dir.current_is_dir():
+			r_dirs.append("%s/%s" % [p_name, f])
+		if f.ends_with("_test.gd"):
+			tests.append("%s/%s" % [p_name, f])
+		f = dir.get_next()
+	dir.list_dir_end()
